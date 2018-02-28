e9c16cf525cd511e750a6237f5e81540e42c693a
jockyxu1122 <yx1366@nyu.edu>
2018-02-28 12:07:28 -0500

    Moved Paths' description to README.md

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Inventory Team
+
+
+Paths:
+------
+GET /
+- Returns useful information about the service.
+
+GET /inventory
+- Returns all the product information in the Inventory.
+
+GET /inventory/{prod_id}
+- Returns production information about a certain product.
+
+POST /inventory
+- Adds a new product with its information to the inventory.
+
+PUT /inventory/{prod_id}
+- Updates information of a product.
+
+DELETE /inventory/{prod_id}
+- Deletes product information with the given product id.
+
+GET /inventory?{prod_name|quantity|condition=val}
+- Returns all products' information meeting given requirement.
+
+PUT /inventory/{prod_id}/restock
+- An action triggers restocking for a product.
--- server.py
+++ server.py
@@ -1,31 +1,5 @@
 """
 Inverntory Management System Service
-
-Paths:
-------
-GET /
-- Returns useful information about the service.
-
-GET /inventory
-- Returns all the product information in the Inventory.
-
-GET /inventory/{prod_id}
-- Returns production information about a certain product.
-
-POST /inventory
-- Adds a new product with its information to the inventory.
-
-PUT /inventory/{prod_id}
-- Updates information of a product.
-
-DELETE /inventory/{prod_id}
-- Deletes product information with the given product id.
-
-GET /inventory?{prod_name|quantity|condition=val}
-- Returns all products' information meeting given requirement.
-
-PUT /inventory/{prod_id}/restock
-- An action triggers restocking for a product.
 """
 
 from flask import Flask, jsonify, url_for, make_response, request
