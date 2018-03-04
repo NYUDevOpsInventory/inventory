8b455a129418a59dcab2f08e9775d4b9b94d3db2
Majia <majiadomain@gmail.com>
2018-03-03 22:12:40 -0500

    nit

--- tests/test_prod_info.py
+++ tests/test_prod_info.py
@@ -207,29 +207,22 @@ class TestProductInformation(unittest.TestCase):
 
     def test_query_by_prod_name(self):
         """ Test query by the product name """
-        prod1 = ProductInformation(prod_id=1234, prod_name="foo")
-        prod1.save()
-
-        prod2 = ProductInformation(prod_id=4321, prod_name="bar")
-        prod2.save()
+        ProductInformation(prod_id=1234, prod_name="foo").save()
+        ProductInformation(prod_id=4321, prod_name="bar").save()
 
         result = ProductInformation.find_by_name("foo")
         self.assertEqual(1, len(result))
         self.assertEqual(1234, result[0].prod_id)
 
-        prod3 = ProductInformation(prod_id=5678, prod_name="foo")
-        prod3.save()
+        ProductInformation(prod_id=5678, prod_name="foo").save()
 
         result = ProductInformation.find_by_name("foo")
         self.assertEqual(2, len(result))
 
     def test_query_by_prod_quantity(self):
         """ Test query by the product quantity """
-        prod1 = ProductInformation(prod_id=1234, new_qty=1, used_qty=2, open_boxed_qty=3)
-        prod1.save()
-
-        prod2 = ProductInformation(prod_id=4321, new_qty=5, used_qty=1, open_boxed_qty=2)
-        prod2.save()
+        ProductInformation(prod_id=1234, new_qty=1, used_qty=2, open_boxed_qty=3).save()
+        ProductInformation(prod_id=4321, new_qty=5, used_qty=1, open_boxed_qty=2).save()
 
         result = ProductInformation.find_by_quantity(1)
         self.assertEqual(0, len(result))
@@ -244,14 +237,9 @@ class TestProductInformation(unittest.TestCase):
 
     def test_query_by_prod_condition(self):
         """ Test query by the product condition """
-        prod1 = ProductInformation(prod_id=1234, new_qty=1, used_qty=0, open_boxed_qty=0)
-        prod1.save()
-
-        prod2 = ProductInformation(prod_id=4321, new_qty=0, used_qty=2, open_boxed_qty=0)
-        prod2.save()
-
-        prod3 = ProductInformation(prod_id=5678, new_qty=1, used_qty=0, open_boxed_qty=3)
-        prod3.save()
+        ProductInformation(prod_id=1234, new_qty=1, used_qty=0, open_boxed_qty=0).save()
+        ProductInformation(prod_id=4321, new_qty=0, used_qty=2, open_boxed_qty=0).save()
+        ProductInformation(prod_id=5678, new_qty=1, used_qty=0, open_boxed_qty=3).save()
 
         result = ProductInformation.find_by_condition("new")
         self.assertEqual(2, len(result))
