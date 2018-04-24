4ea55ff18b458365a83662a0bd7ece0777889ff3
jockyxu1122 <yx1366@nyu.edu>
2018-04-24 17:26:29 +0000

    move swagger config to config.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,19 +9,3 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 from app import server, models
-from flasgger import Swagger
-
-# Configurue Swagger
-app.config['SWAGGER'] = {
-    "swagger_version": "2.0",
-    "specs": [
-        {
-            "version": "1.0.0",
-            "title": "Inventory",
-            "description": "This is an Inventory server.",
-            "endpoint": 'v1_spec',
-            "route": '/v1/spec'
-        }
-    ]
-}
-Swagger(app)
--- config.py
+++ config.py
@@ -1,6 +1,23 @@
 import logging
 from app.vcap_services import get_database_uri
+from app import app
+from flasgger import Swagger
 
 SQLALCHEMY_DATABASE_URI = get_database_uri()
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 LOGGING_LEVEL = logging.INFO
+
+# Configurue Swagger
+app.config['SWAGGER'] = {
+    "swagger_version": "2.0",
+    "specs": [
+        {
+            "version": "1.0.0",
+            "title": "Inventory",
+            "description": "This is an Inventory server.",
+            "endpoint": 'v1_spec',
+            "route": '/v1/spec'
+        }
+    ]
+}
+Swagger(app)
