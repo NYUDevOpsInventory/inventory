c0316e530ab90cf95ae72bcf3d9006dcea5b39b6
Mingshi Peng <mingshipeng@172-16-202-29.DYNAPOOL.NYU.EDU>
2018-04-26 17:40:30 -0400

    add BDD test case for Read

--- features/inventory.feature
+++ features/inventory.feature
@@ -2,3 +2,20 @@ Feature: The Inventory service back-end
     As a Inventory owner
     I need a RESTful product management service
     So that I can keep track of all my product information
+
+Background:
+    Given the following products in inventory
+        | prod_id | prod_name | new_qty | used_qty | open_boxed_qty | restock_level | restock_amt |
+        |  1      | iPod      | 1       | 1        | 3              | 4             | 8           |
+        |  2      | Macbook   | 2       | 2        | 2              | 5             | 9           |
+        |  3      | iPhone    | 3       | 3        | 1              | 6             | 10          |
+
+Scenario: Retrive an ProductInformation
+    When I visit the "Home Page"
+    AND I set the "prod_id" to "2"
+    AND I press the "retrieve" Button
+    Then I should see "2" in the "new_qty" field
+    AND I should see "2" in the "used_qty" field
+    AND I should see "2" in the "open_boxed_qty" field
+    AND I should see "5" in the "restock_level" field
+    AND I should see "9" in the "restock_amt" field
--- features/steps/inventory_steps.py
+++ features/steps/inventory_steps.py
@@ -6,14 +6,61 @@ Steps file for inventory.feature.
 from os import getenv
 import json
 import requests
-from behave import *
-from compare import expect, ensure
+from compare import expect
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
-from app import server
 
 WAIT_SECONDS = 30
 BASE_URL = getenv('BASE_URL', 'http://localhost:5000/')
 
-# BDD tests below.
+@given(u'the following products in inventory')
+def step_impl(context):
+    """ Delete all ProductInformation and load new ones """
+    inventory_url = context.base_url + '/inventory'
+    # first delete all mentioned products (if exist)
+    for row in context.table:
+        context.resp = requests.delete(inventory_url + '/' + row['prod_id'])
+        expect(context.resp.status_code).to_equal(200)
+
+    # then create all mentioned products
+    headers = {'Content-Type': 'application/json'}
+    for row in context.table:
+        data = {
+            "prod_id":          int(row['prod_id']),
+            "prod_name":        row['prod_name'],
+            "new_qty":          int(row['new_qty']),
+            "used_qty":         int(row['used_qty']),
+            "open_boxed_qty":   int(row['open_boxed_qty']),
+            "restock_level":    int(row['restock_level']),
+            "restock_amt":      int(row['restock_amt']),
+        }
+        payload = json.dumps(data)
+        context.resp = requests.post(inventory_url, data=payload, headers=headers)
+        expect(context.resp.status_code).to_equal(201)
+
+@when(u'I visit the "Home Page"')
+def step_impl(context):
+    """ Make a call to the base URL """
+    context.driver.get(context.base_url)
+
+@when(u'I set the "{element_id}" to "{text_string}"')
+def step_impl(context, element_id, text_string):
+    element = context.driver.find_element_by_id(element_id)
+    element.clear()
+    element.send_keys(text_string)
+
+@when(u'I press the "{button}" Button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    context.driver.find_element_by_id(button_id).click()
+
+@then(u'I should see "{text_string}" in the "{element_id}" field')
+def step_impl(context, text_string, element_id):
+    found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+        expected_conditions.text_to_be_present_in_element_value(
+            (By.ID, element_id),
+            text_string
+        )
+    )
+    expect(found).to_be(True)
